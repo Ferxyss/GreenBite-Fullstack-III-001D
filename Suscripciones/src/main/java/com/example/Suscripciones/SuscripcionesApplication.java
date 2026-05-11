1c255ed262e02baa6ecaf2a316b85735d0c6d805
Ferxys <166613349+Ferxyss@users.noreply.github.com>
2026-05-11 15:38:29 -0400

    chore: initial commit with monorepo structure

---
+++ Suscripciones/src/main/java/com/example/Suscripciones/SuscripcionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Suscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuscripcionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuscripcionesApplication.class, args);
+	}
+
+}
---
+++ Suscripciones/src/test/java/com/example/Suscripciones/SuscripcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Suscripciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuscripcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
